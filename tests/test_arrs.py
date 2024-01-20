7537ec6f41114832d08e4f402642433cf485d906
Valeria Mazurova <valeryfox90@gmail.com>
2024-01-20 16:29:29 +0500

    Исправлен тест2

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -7,5 +7,5 @@ def test_get():
 
 
 def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
+    assert arrs.my_slice([1, 2, 3, 4], 1, 4) == [2, 3, 4]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
